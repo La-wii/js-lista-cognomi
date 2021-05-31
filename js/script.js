af273c8bb79ee0af765307a2058b5bf17b3a8b8c
La-wii <sentilavale@gmail.com>
2021-05-31 14:50:35 +0200

    for if email == lista + else

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 // Chiedi all’utente la sua email
-var email = prompt('Inserisci la tua e-mail')
+var email = prompt('Inserisci la tua e-mail');
 
 
 // Controlla che sia nella lista di chi può accedere
 
-var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"]
+var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"];
+
+
+for (i = 0; i < lista.length; i++) {
+    if (email == lista[i]) {
+        console.log('Puoi accedere');
+    } else {
+        console.log('Non puoi accedere');
+    }
+
+}   
 
 
 
