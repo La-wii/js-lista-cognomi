bf6b1bc9e58386b875de0d3c92c39cf2ed1191b4
La-wii <sentilavale@gmail.com>
2021-05-31 15:20:47 +0200

    var cognome + var listaCognomi + listaCognomi.sort

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Chiedi all’utente la sua email
-var email = prompt('Inserisci la tua e-mail');
+// var email = prompt('Inserisci la tua e-mail');
 
 
 // Controlla che sia nella lista di chi può accedere
 
-var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"];
+// var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"];
 
 
 // for (i = 0; i < lista.length; i++) {
@@ -18,11 +18,28 @@ var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"];
 
 // Stampa un messaggio appropriato sull’esito del controllo.
 
-for (i = 0; i < lista.length; i++) {
-    if (email == lista[i]) {
-        alert('Puoi accedere');
-    } else {
-        alert('Non puoi accedere');
-    }
+// for (i = 0; i < lista.length; i++) {
+//     if (email == lista[i]) {
+//         alert('Puoi accedere');
+//     } else {
+//         alert('Non puoi accedere');
+//     }
 
-}   
+// }   
+
+// SECONDO ESERCIZIO
+
+// chiedi all’utente il cognome
+var cognome = prompt('Scrvi il tuo cognome');
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
+var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+listaCognomi.push(cognome);
+console.log(listaCognomi);
+// stampa la lista ordinata alfabeticamente
+listaCognomi.sort();
+console.log(listaCognomi);
+
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
