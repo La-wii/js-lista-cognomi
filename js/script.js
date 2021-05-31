2e9ceeec96c900482f04da08ff7841381f9d315b
La-wii <sentilavale@gmail.com>
2021-05-31 14:17:07 +0200

    var email + var lista

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente la sua email
+var email = prompt('Inserisci la tua e-mail')
+
+
+// Controlla che sia nella lista di chi può accedere
+
+var lista = ["rossi@gmail.com", "gialli@gmail.com", "verdi@gmail.com"]
+
+
+
+// Stampa un messaggio appropriato sull’esito del controllo.
